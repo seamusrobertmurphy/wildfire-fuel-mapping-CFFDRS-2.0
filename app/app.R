74a9b438c4be7601e125d6959cc7eeea685b320d
seamusrobertmurphy <seamusrobertmurphy@gmail.com>
2022-04-05 08:12:11 -0700

    commit post-deadline

--- app/app.R
+++ app/app.R
@@ -1,15 +1,10 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+
+install.packages(c("maps", "mapproj"))
 
 library(shiny)
+library(bcmaps)
+available_layers()
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
